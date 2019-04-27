e1534f9f45919fe16e668d1b91e7c781370b2e6c
Andy Davis <me@andy-davis.co.uk>
2019-04-28 00:48:36 +0100

    minor refactor

--- src/test/kotlin/GameApiClientTests.kt
+++ src/test/kotlin/GameApiClientTests.kt
@@ -59,7 +59,7 @@ class GameApiTests {
         );
 
         override fun getBalance(id: String, guid: String): GetBalanceResponse {
-            TODO("not implemented") 
+            TODO("not implemented")
         }
 
         override fun enter(id: String, guid: String, key: String, platform: String, gameName: String): EnterResponse {
